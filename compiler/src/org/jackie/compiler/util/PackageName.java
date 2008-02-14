c3834e7f577aa6c8715f2a9e78611e6414304de1
Patrik Beno <patrik.beno@gmail.com>
2008-02-14 02:01:21 +0100

    [compiler] utils: ClassName, PackageName

---
+++ compiler/src/org/jackie/compiler/util/PackageName.java
@@ -0,0 +1,43 @@
+package org.jackie.compiler.util;
+
+/**
+ * @author Patrik Beno
+ */
+public class PackageName {
+
+	String fqname;
+
+	PackageName parent;
+	String name;
+
+	public PackageName(String fqname) {
+		this.fqname = fqname;
+	}
+
+	public String getFQName() {
+		return fqname;
+	}
+
+	public String getName() {
+		if (name != null) {
+			return name;
+		}
+
+		int i = fqname.lastIndexOf('.');
+		if (i == -1) {
+			i = 0;
+		}
+
+		name = fqname.substring(i);
+		return name;
+	}
+
+	public PackageName getParent() {
+		int offset = fqname.length() - getName().length() - 1;
+		if (offset > 0) {
+			parent = new PackageName(fqname.substring(0, offset));
+		}
+		return parent;
+	}
+
+}
