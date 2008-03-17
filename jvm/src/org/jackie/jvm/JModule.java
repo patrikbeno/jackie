eebcb318df213c22a0bf03dff9785799d9a4a346
Patrik Beno <patrik.beno@gmail.com>
2008-03-17 12:10:35 +0100

    [jvm] cleanup: removed obsolete classes (JProject, JModule, JResource, TypeRegistry - will be declared in another, higher-level module)

--- jvm/src/org/jackie/jvm/JModule.java
+++
@@ -1,9 +0,0 @@
-package org.jackie.jvm;
-
-import org.jackie.jvm.props.Named;
-
-/**
- * @author Patrik Beno
- */
-public interface JModule extends JNode, Named {
-}
--- jvm/src/org/jackie/jvm/JResource.java
+++
@@ -1,9 +0,0 @@
-package org.jackie.jvm;
-
-import org.jackie.jvm.props.Named;
-
-/**
- * @author Patrik Beno
- */
-public interface JResource extends JNode, Named {
-}
