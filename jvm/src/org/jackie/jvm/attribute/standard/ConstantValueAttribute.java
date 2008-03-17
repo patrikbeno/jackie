d3d25594170f1ed5c7435a7560082f5c79769c55
Patrik Beno <patrik.beno@gmail.com>
2008-03-17 13:42:27 +0100

    [jvm] javadoc

--- jvm/src/org/jackie/jvm/attribute/standard/ConstantValueAttribute.java
+++ jvm/src/org/jackie/jvm/attribute/standard/ConstantValueAttribute.java
@@ -3,8 +3,14 @@ package org.jackie.jvm.attribute.standard;
 import org.jackie.jvm.attribute.JAttribute;
 
 /**
+ * @deprecated Compiler must support this attribute for backward compatibility but its usage is
+ *             discouraged. Any field initialization should be done in a dedicated initializer
+ *             (method). Compiler implementation should convert all occurences of this attribute
+ *             into mentioned initializer
+ * 
  * @author Patrik Beno
  */
+@Deprecated
 public class ConstantValueAttribute implements JAttribute {
 
 	protected Object value;
