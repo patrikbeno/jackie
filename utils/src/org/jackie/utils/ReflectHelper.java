993e62cec117a9c0a1f04ac1f9374876bdfc99e2
Patrik Beno <patrik.beno@gmail.com>
2008-06-26 01:51:22 +0200

    [utils] new: ReflectHelper

---
+++ utils/src/org/jackie/utils/ReflectHelper.java
@@ -0,0 +1,51 @@
+package org.jackie.utils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author Patrik Beno
+ */
+public class ReflectHelper<T> {
+
+	static public <T> ReflectHelper<T> reflectHelper(Class<T> type) {
+		return new ReflectHelper<T>(type);
+	}
+
+	Class<T> type;
+
+	public ReflectHelper(Class<T> type) {
+		this.type = type;
+	}
+
+	public ConstructorHelper getConstructor(Class ... args) {
+		return new ConstructorHelper<T>(type, args);
+	}
+
+	public class ConstructorHelper<T> {
+
+		Constructor<T> constructor;
+
+		public ConstructorHelper(Class<T> type, Class ... args) {
+			try {
+				constructor = type.getConstructor(args);
+			} catch (NoSuchMethodException e) {
+				throw Assert.assertFailed(e, "No such constructor");
+			}
+		}
+
+		public T newInstance(Object ... args) {
+			try {
+				T instance = constructor.newInstance(args);
+				return instance;
+			} catch (InstantiationException e) {
+				throw Assert.assertFailed(e, "Error in newInstance()");
+			} catch (IllegalAccessException e) {
+				throw Assert.assertFailed(e, "Error in newInstance()");
+			} catch (InvocationTargetException e) {
+				throw Assert.assertFailed(e, "Error in newInstance()"); // fixme handle/support user exceptions
+			}
+		}
+
+	}
+}
