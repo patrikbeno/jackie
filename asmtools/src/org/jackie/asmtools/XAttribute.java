b2ccf2a9754bb59c150e60162570bc377499d5ba
Patrik Beno <patrik.beno@gmail.com>
2008-08-22 03:49:38 +0200

    [asmtools] new: XAttribute (allows setting value - workaround for ASM API limitation)

---
+++ asmtools/src/org/jackie/asmtools/XAttribute.java
@@ -0,0 +1,30 @@
+package org.jackie.asmtools;
+
+import org.objectweb.asm.Attribute;
+import org.jackie.utils.Assert;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author Patrik Beno
+ */
+public class XAttribute extends Attribute {
+
+	public XAttribute(String type) {
+		super(type);
+	}
+
+	public XAttribute(String type, byte[] value) {
+		super(type);
+		try {
+			Field f = Attribute.class.getDeclaredField("value");
+			f.setAccessible(true);
+			f.set(this, value);
+		} catch (NoSuchFieldException e) {
+			throw Assert.notYetHandled(e);
+		} catch (IllegalAccessException e) {
+			throw Assert.notYetHandled(e);
+		}
+	}
+
+}
