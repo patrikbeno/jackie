b19f81c8a14b32ffbde16c52016c8b49e944126e
Patrik Beno <patrik.beno@gmail.com>
2008-08-22 09:22:35 +0200

    [jclassfile] Access: completed list of all available flags

--- jclassfile/src/org/jackie/jclassfile/flags/Access.java
+++ jclassfile/src/org/jackie/jclassfile/flags/Access.java
@@ -6,18 +6,25 @@ package org.jackie.jclassfile.flags;
 public enum Access {
 
    PUBLIC(0x0001),
+   PRIVATE(0x0002),
+   PROTECTED(0x0004),
+   STATIC(0x0008),
    FINAL(0x0010),
-   SUPER(0x0020),
-   INTERFACE(0x0200),
+   SUPER(0x0020), SYNCHRONIZED(0x0020),
+	VOLATILE(0x0040),
+	TRANSIENT(0x0080),
+	NATIVE(0x0100),
+	INTERFACE(0x0200),
    ABSTRACT(0x0400),
+   STRICT(0x0800),
 
-   ;
+	;
 
    private int value;
 
-   private Access(int value) {
+	private Access(int value) {
       this.value = value;
-   }
+	}
 
    public int value() {
       return value;
