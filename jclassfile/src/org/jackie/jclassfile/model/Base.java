9547ee42fd4bd9ce57ffa6ce097934541c6fbba2
Patrik Beno <patrik.beno@gmail.com>
2008-08-22 11:15:11 +0200

    [jclassfile] Base: common data length management routines

--- jclassfile/src/org/jackie/jclassfile/model/Base.java
+++ jclassfile/src/org/jackie/jclassfile/model/Base.java
@@ -1,5 +1,7 @@
 package org.jackie.jclassfile.model;
 
+import static org.jackie.utils.Assert.doAssert;
+
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
@@ -15,4 +17,24 @@ public abstract class Base {
 	abstract
 	public void save(DataOutput out) throws IOException;
 
+
+	protected int readLength(DataInput in) throws IOException {
+		return readLength(in, null);
+	}
+
+	protected int readLength(DataInput in, Integer expected) throws IOException {
+		int len = in.readUnsignedShort();
+		if (expected != null) {
+			doAssert(len == expected, "Invalid length: %s. Expected: %s", len, expected);
+		}
+		return len;
+	}
+
+	protected void writeLength(DataOutput out, int length) throws IOException {
+		out.writeShort(length);
+	}
+
+	protected void assertLength(int actual, int expected) {
+		doAssert(actual==expected, "Actual and expected lengths do not match (actual=%s, expected=%s)", actual, expected);
+	}
 }
