253c01c98337a6536017200e8fff6e4ca747ce93
Patrik Beno <patrik.beno@gmail.com>
2008-08-26 23:14:27 +0200

    [compiler_impl] new: ByteCodeBuilder.getTypeDescriptor(), getMethodDescriptor()

--- compiler_impl/src/org/jackie/compiler_impl/bytecode/ByteCodeBuilder.java
+++ compiler_impl/src/org/jackie/compiler_impl/bytecode/ByteCodeBuilder.java
@@ -3,9 +3,20 @@ package org.jackie.compiler_impl.bytecode;
 import static org.jackie.context.ContextManager.context;
 import org.jackie.jvm.JClass;
 import org.jackie.jvm.JNode;
+import org.jackie.jvm.structure.JMethod;
+import org.jackie.jvm.structure.JParameter;
+import org.jackie.jvm.extension.builtin.PrimitiveType;
+import org.jackie.jvm.extension.builtin.ArrayType;
+import org.jackie.jvm.spi.JModelHelper;
 import static org.jackie.utils.Assert.typecast;
 import org.jackie.jclassfile.util.ClassNameHelper;
+import org.jackie.jclassfile.util.TypeDescriptor;
+import org.jackie.jclassfile.util.MethodDescriptor;
+import org.jackie.jclassfile.model.Type;
 import org.jackie.compiler.spi.Compilable;
+
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @author Patrik Beno
@@ -29,7 +40,25 @@ public abstract class ByteCodeBuilder {
 		return ClassNameHelper.toBinaryClassName(jclass.getFQName());
 	}
 
-	protected void compile(JNode jnode) {
-		typecast(jnode, Compilable.class).compile();
+	protected TypeDescriptor getTypeDescriptor(JClass jclass) {
+		PrimitiveType primitive = jclass.extensions().get(PrimitiveType.class);
+		ArrayType array = jclass.extensions().get(ArrayType.class);
+		TypeDescriptor d = new TypeDescriptor(
+				primitive != null ? Type.forClass(primitive.getPrimitiveClass()) : Type.CLASS,
+				array != null ? array.getDimensions() : 0,
+				primitive == null ? toBinaryClassName(jclass) : null
+		);
+		return d;
 	}
+
+	protected MethodDescriptor getMethodDescriptor(JMethod jmethod) {
+		List<TypeDescriptor> params = new ArrayList<TypeDescriptor>();
+		for (JParameter p : jmethod.getParameters()) {
+			params.add(getTypeDescriptor(p.getType()));
+		}
+		TypeDescriptor rtype = getTypeDescriptor(jmethod.getType());
+
+		return new MethodDescriptor(rtype, params);
+	}
+
 }
