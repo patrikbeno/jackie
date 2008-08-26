f819cfdad575d7a047752ed0c1b705e14c576316
Patrik Beno <patrik.beno@gmail.com>
2008-08-27 00:59:50 +0200

    [test] fix: TestCase: broken import on refactoring; also minor update in initialization

--- test/src/org/jackie/test/java5/annotation/TestCase.java
+++ test/src/org/jackie/test/java5/annotation/TestCase.java
@@ -3,12 +3,13 @@ package org.jackie.test.java5.annotation;
 import org.jackie.compiler.extension.ExtensionManager;
 import org.jackie.compiler.filemanager.FileManager;
 import org.jackie.compiler.typeregistry.TypeRegistry;
-import org.jackie.compiler_impl.ExtensionManagerImpl;
+import org.jackie.compiler_impl.ext.ExtensionManagerImpl;
 import org.jackie.compiler_impl.filemanager.ClassPathFileManager;
 import org.jackie.compiler_impl.typeregistry.JClassRegistry;
 import org.jackie.compiler_impl.typeregistry.MultiRegistry;
 import org.jackie.compiler_impl.typeregistry.PrimitiveTypeRegistry;
 import static org.jackie.context.ContextManager.*;
+import org.jackie.utils.Assert;
 
 /**
  * @author Patrik Beno
@@ -20,12 +21,16 @@ public abstract class TestCase {
 	protected ExtensionManager extmanager;
 
 	{
-		filemanager = new ClassPathFileManager();
-		typeregistry = new MultiRegistry(
-				new PrimitiveTypeRegistry(),
-				new JClassRegistry(filemanager)
-		);
-		extmanager = new ExtensionManagerImpl();
+		try {
+			filemanager = new ClassPathFileManager();
+			typeregistry = new MultiRegistry(
+					new PrimitiveTypeRegistry(),
+					new JClassRegistry(filemanager)
+			);
+			extmanager = new ExtensionManagerImpl();
+		} catch (Throwable t) {
+			throw Assert.unexpected(t);
+		}
 	}
 
 	protected void run(Runnable r) {
