3c0762a739b2d8f0c56ec6b29af48f43939d9c5e
Patrik Beno <patrik.beno@gmail.com>
2008-09-05 02:44:37 +0200

    [java5] minor cleanup (JAnnotationImpl)

--- java5/src/org/jackie/java5/annotation/impl/JAnnotationImpl.java
+++ java5/src/org/jackie/java5/annotation/impl/JAnnotationImpl.java
@@ -20,6 +20,7 @@ import org.jackie.jvm.JClass;
 import org.jackie.jvm.JNode;
 import org.jackie.jvm.extension.builtin.ArrayType;
 import org.jackie.jvm.spi.AbstractJNode;
+import org.jackie.jvm.spi.JModelHelper;
 import org.jackie.utils.Assert;
 import static org.jackie.utils.Assert.typecast;
 import org.jackie.utils.ClassName;
@@ -47,84 +48,13 @@ public class JAnnotationImpl extends AbstractJNode implements JAnnotation, Compi
 	public JAnnotationImpl(JNode owner, org.jackie.jclassfile.attribute.anno.Annotation anno) {
 		super(owner);
 
-		TypeDescriptor desc = anno.type();
-		String bname = ClassNameHelper.toJavaClassName(desc.getTypeName());
-		ClassName clsname = new ClassName(bname, desc.getDimensions());
-
-		JClass jclass = context(TypeRegistry.class).getJClass(clsname);
+		JClass jclass = getJClass(anno.type());
 		AnnotationType type = jclass.extensions().get(AnnotationType.class);
-
-		init(type, owner);
 
 		for (ElementValue evalue : anno.elements()) {
 			JAnnotationElementValue value = new JAnnotationElementValueImpl(this, type.getElement(evalue.name()));
 			edit().addAttributeValue(value);
 		}
-	}
-
-	public JNode owner() {
-		throw Assert.notYetImplemented(); // todo implement this
-	}
-
-	void init(AnnotationType type, Object owner) {
-		assert type != null;
-		assert owner != null;
-//		assert owner instanceof Extensible && ((Extensible)owner).extensions().supports(JAnnotations.class) || owner instanceof JAnnotation;
-		assert owner instanceof JAnnotations || owner instanceof JAnnotation;
-//		this.owner = owner;
-		this.type = type;
-	}
-
-
-	JClass getJClass(TypeDescriptor desc) {
-		String bname = ClassNameHelper.toJavaClassName(desc.getTypeName());
-		ClassName clsname = new ClassName(bname, desc.getDimensions());
-		JClass jclass = context(TypeRegistry.class).getJClass(clsname);
-		return jclass;
-	}
-
-	String getClassName(TypeDescriptor desc) {
-		return toJavaClassName(desc.getTypeName());
-	}
-
-	Object convert(JClass jclass, ElementValue evalue) {
-
-		switch (evalue.tag()) {
-			case BYTE:
-			case CHAR:
-			case DOUBLE:
-			case FLOAT:
-			case INT:
-			case LONG:
-			case SHORT:
-			case BOOLEAN:
-			case STRING:
-				return typecast(evalue, ElementValue.ConstElementValue.class).value();
-			case ENUM:
-				EnumElementValue enumvalue = typecast(evalue, EnumElementValue.class);
-				return new EnumProxy(getClassName(enumvalue.type()), enumvalue.value());
-			case CLASS:
-				ClassElementValue classvalue = typecast(evalue, ClassElementValue.class);
-				return new ClassProxy(getClassName(classvalue.type()));
-			case ANNOTATION:
-				AnnoElementValue annovalue = typecast(evalue, AnnoElementValue.class);
-				return new JAnnotationImpl(this, annovalue.annotation());
-			case ARRAY:
-				ArrayElementValue arrayvalue = typecast(evalue, ArrayElementValue.class);
-				ArrayType array = jclass.extensions().get(ArrayType.class);
-				return convertArray(array.getComponentType(), arrayvalue);
-		   default:
-				throw Assert.invariantFailed(evalue.tag());
-		}
-
-	}
-
-	List convertArray(JClass jclass, ArrayElementValue arrayvalue) {
-		List<Object> converted = new ArrayList<Object>(arrayvalue.values().size());
-		for (ElementValue evalue : arrayvalue.values()) {
-			converted.add(convert(jclass, evalue));
-		}
-		return converted;
 	}
 
 
@@ -157,7 +87,7 @@ public class JAnnotationImpl extends AbstractJNode implements JAnnotation, Compi
 	}
 
 	public boolean isEditable() {
-		throw Assert.notYetImplemented(); // todo implement this 
+		return JModelHelper.isEditable(this);
 	}
 
 	public Editor edit() {
@@ -174,6 +104,57 @@ public class JAnnotationImpl extends AbstractJNode implements JAnnotation, Compi
 				return JAnnotationImpl.this;
 			}
 		};
+	}
+
+	public void compile() {
+		throw Assert.notYetImplemented(); // todo implement compile()
+	}
+
+	JClass getJClass(TypeDescriptor desc) {
+		String bname = ClassNameHelper.toJavaClassName(desc.getTypeName());
+		ClassName clsname = new ClassName(bname, desc.getDimensions());
+		JClass jclass = context(TypeRegistry.class).getJClass(clsname);
+		return jclass;
+	}
+
+	Object convert(JClass jclass, ElementValue evalue) {
+
+		switch (evalue.tag()) {
+			case BYTE:
+			case CHAR:
+			case DOUBLE:
+			case FLOAT:
+			case INT:
+			case LONG:
+			case SHORT:
+			case BOOLEAN:
+			case STRING:
+				return typecast(evalue, ElementValue.ConstElementValue.class).value();
+			case ENUM:
+				EnumElementValue enumvalue = typecast(evalue, EnumElementValue.class);
+				return new EnumProxy(toJavaClassName(enumvalue.type()), enumvalue.value());
+			case CLASS:
+				ClassElementValue classvalue = typecast(evalue, ClassElementValue.class);
+				return new ClassProxy(toJavaClassName(classvalue.type()));
+			case ANNOTATION:
+				AnnoElementValue annovalue = typecast(evalue, AnnoElementValue.class);
+				return new JAnnotationImpl(this, annovalue.annotation());
+			case ARRAY:
+				ArrayElementValue arrayvalue = typecast(evalue, ArrayElementValue.class);
+				ArrayType array = jclass.extensions().get(ArrayType.class);
+				return convertArray(array.getComponentType(), arrayvalue);
+		   default:
+				throw Assert.invariantFailed(evalue.tag());
+		}
+
+	}
+
+	List convertArray(JClass jclass, ArrayElementValue arrayvalue) {
+		List<Object> converted = new ArrayList<Object>(arrayvalue.values().size());
+		for (ElementValue evalue : arrayvalue.values()) {
+			converted.add(convert(jclass, evalue));
+		}
+		return converted;
 	}
 
 	public java.lang.annotation.Annotation proxy() {
@@ -198,8 +179,5 @@ public class JAnnotationImpl extends AbstractJNode implements JAnnotation, Compi
 			throw Assert.notYetHandled(e);
 		}
 
-	}
-
-	public void compile() {
 	}
 }
