eb5b534ed161388d8752a21781adfc1293a66ebb
Patrik Beno <patrik.beno@gmail.com>
2008-09-22 22:20:04 +0200

    [test] ClassFileTest: loadAndSave(), generateComplexClass()

--- test/src/org/jackie/test/jclassfile/ClassFileTest.java
+++ test/src/org/jackie/test/jclassfile/ClassFileTest.java
@@ -1,6 +1,16 @@
 package org.jackie.test.jclassfile;
 
 import org.jackie.jclassfile.model.ClassFile;
+import org.jackie.jclassfile.model.FieldInfo;
+import org.jackie.jclassfile.model.Type;
+import org.jackie.jclassfile.model.AttributeInfo;
+import org.jackie.jclassfile.util.TypeDescriptor;
+import org.jackie.jclassfile.util.Helper;
+import org.jackie.jclassfile.flags.Access;
+import org.jackie.jclassfile.attribute.GenericAttribute;
+import org.jackie.utils.Assert;
+import static org.jackie.utils.Assert.expected;
+import org.jackie.jvm.spi.JModelHelper;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayInputStream;
@@ -10,11 +20,24 @@ import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 
 /**
  * @author Patrik Beno
  */
 public class ClassFileTest {
+
+	@Test
+	public void loadAndSave() throws Exception {
+		byte[] original = Sample.bytecode();
+
+		ClassFile cf = new ClassFile();
+		cf.load(new DataInputStream(new ByteArrayInputStream(original)));
+
+		byte[] saved = cf.toByteArray();
+
+		expected(true, Arrays.equals(original, saved), "original != saved");
+	}
 
 	@Test
 	public void generateSimpleClass() throws IOException {
@@ -76,6 +99,29 @@ public class ClassFileTest {
 
 		new ClassLoader(null) {{
 				defineClass(cls.getName(), bytes, 0, bytes.length);
+		}};
+	}
+
+	@Test
+	public void generateComplexClass() {
+		final ClassFile cf = new ClassFile();
+		cf.classname("test/Simple").superclass("java/lang/Object");
+		cf.addInterface("java/io/Serializable");
+
+		FieldInfo f = new FieldInfo(cf);
+		f.name("sample");
+		f.typeDescriptor(new TypeDescriptor(Type.CLASS, 0, "java/lang/String"));
+		f.flags().set(Access.PROTECTED);
+		cf.addField(f);
+
+		GenericAttribute a = new GenericAttribute(f, cf.pool().factory().getUtf8("AnyAttribute"));
+		a.data(new byte[1024]);
+		f.addAttribute(a);
+
+		final byte[] bytes = cf.toByteArray();
+
+		new ClassLoader(null) {{
+				defineClass("test.Simple", bytes, 0, bytes.length);
 		}};
 	}
 }
--- test/src/org/jackie/test/jclassfile/Sample.java
+++ test/src/org/jackie/test/jclassfile/Sample.java
@@ -1,7 +1,24 @@
 package org.jackie.test.jclassfile;
 
+import static org.jackie.utils.Assert.NOTNULL;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * @author Patrik Beno
  */
 public class Sample {
+
+	static public byte[] bytecode() throws IOException {
+		URL url = NOTNULL(Sample.class.getResource(Sample.class.getSimpleName() + ".class"));
+		URLConnection con = url.openConnection();
+		byte[] bytes = new byte[con.getContentLength()];
+		DataInputStream in = new DataInputStream(con.getInputStream());
+		in.readFully(bytes);
+		return bytes;
+	}
+
 }
