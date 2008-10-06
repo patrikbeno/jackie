ad9b828db0a3ac41d072efba3a2401fdb6cfb877
Patrik Beno <patrik.beno@gmail.com>
2008-10-06 22:45:28 +0200

    [wiki] fix: (runwiki.sh) don't delete existing data directory (backup if exists)

--- .wiki/runwiki.sh
+++ .wiki/runwiki.sh
@@ -5,7 +5,12 @@ cd $(dirname $0)
 . .functions.sh
 
 echo "Preparing data..."
-deleteData
+if [ -d data ]; then
+	fname="data_$(date +%Y%m%d_%H%M).tgz"
+	echo "!!! data directory exists; backing up: $fname"
+	tar czf $fname data
+	deleteData
+fi
 unpackData
 
 echo "Starting WIKI..."
