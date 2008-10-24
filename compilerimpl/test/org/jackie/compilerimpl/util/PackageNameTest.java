fef18e33780f0451e8b91cf318c95ac3810b76d6
Patrik Beno <patrik.beno@gmail.com>
2008-10-24 02:45:56 +0200

    [compilerimpl] fixed PackageNameTest

--- compilerimpl/test/org/jackie/compilerimpl/util/PackageNameTest.java
+++ compilerimpl/test/org/jackie/compilerimpl/util/PackageNameTest.java
@@ -16,7 +16,8 @@ public class PackageNameTest {
 		Assert.expected("reflection", pckgname.getName(), "getName()");
 		Assert.expected("java.lang", pckgname.getParent().getFQName(), "parent.getFQName()");
 		Assert.expected("java", pckgname.getParent().getParent().getFQName(), "parent.parent.getFQName()");
-		Assert.expected(null, pckgname.getParent().getParent().getParent(), "parent up to default package!");
+		Assert.expected(PackageName.DEFAULT, pckgname.getParent().getParent().getParent(), "parent up to default package!");
+		Assert.expected(PackageName.DEFAULT, PackageName.DEFAULT.getParent(), "default's parent");
 	}
 
 
