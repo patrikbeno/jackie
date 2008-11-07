72b779ad716adfd3ab813fcc0bb4aa5e8a351fc5
Patrik Beno <patrik.beno@gmail.com>
2008-11-07 19:26:15 +0100

    [doc] runwiki.sh: use default moin executable location and default port 8080

--- doc/wiki/runwiki.sh
+++ doc/wiki/runwiki.sh
@@ -5,7 +5,7 @@ cd $(dirname $0)
 echo "Starting WIKI..."
 # http://moinmo.in/
 # http://moinmo.in/MoinMoinDownload
-/usr/share/moin/server/moin server standalone --config-dir=. --interface=localhost --port=10086 $*
+moin server standalone --config-dir=. --interface=localhost --port=8080 $*
 
 echo "Deleting runtime data..."
 find data -type d -name cache | xargs rm -rf
