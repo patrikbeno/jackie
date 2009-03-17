96e344de6dd918a99ac6bc2a4cd26ffbefc969c5
Patrik Beno <patrik.beno@gmail.com>
2009-03-18 00:37:59 +0100

    JAC-13 DumpClass updates

--- tools/src/org/jackie/tools/DumpClass.java
+++ tools/src/org/jackie/tools/DumpClass.java
@@ -14,6 +14,9 @@ import org.jackie.utils.DataInputWrapper;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -22,48 +25,36 @@ import java.util.ArrayList;
  */
 public class DumpClass {
 
-	static public void main(String[] args) {
+	static public void main(String[] args) throws FileNotFoundException {
 		File f = new File(args[0]);
-		DumpClass dump = new DumpClass(f);
-		dump.dump();
+		DumpClass.dump(new FileInputStream(f), System.out);
 	}
 
-	File file;
+	static public void dump(InputStream byteCodeInput, PrintStream out) {
+		ClassFile cf = new ClassFile();
+		cf.load(new DataInputWrapper(byteCodeInput));
 
-	public DumpClass(File file) {
-		this.file = file;
-	}
+		print(out, "class %s extends %s implements %s",
+				cf.classname(), cf.superclass(), toNames(cf.interfaces()));
 
-	void dump() {
-		try {
-			ClassFile cf = new ClassFile();
-			cf.load(new DataInputWrapper(new FileInputStream(file)));
+		print(out, "Fields:");
+		for (FieldInfo f : cf.fields()) {
+			print(out, "\t%s : %s", f.name(), f.typeDescriptor().getTypeName());
+		}
 
-			print("class %s extends %s implements %s",
-					cf.classname(), cf.superclass(), toNames(cf.interfaces()));
+		print(out, "Methods:");
+		for (MethodInfo m : cf.methods()) {
+			print(out, "\t%s(%s) : %s", m.name(), toNames2(m.methodDescriptor().getParameterTypes()), m.methodDescriptor().getReturnType().getTypeName());
+			dumpCode(out, m);
+		}
 
-			print("Fields:");
-			for (FieldInfo f : cf.fields()) {
-				print("\t%s : %s", f.name(), f.typeDescriptor().getTypeName());
-			}
-
-			print("Methods:");
-			for (MethodInfo m : cf.methods()) {
-				print("\t%s(%s) : %s", m.name(), toNames2(m.methodDescriptor().getParameterTypes()), m.methodDescriptor().getReturnType().getTypeName());
-				dumpCode(m);
-			}
-
-			print("Attributes");
-			for (AttributeInfo a : cf.attributes()) {
-				print("\t%s @ %s: %s", a.name(), a.owner(), a);
-			}
-
-		} catch (IOException e) {
-			throw Assert.notYetHandled(e);
+		print(out, "Attributes");
+		for (AttributeInfo a : cf.attributes()) {
+			print(out, "\t%s @ %s: %s", a.name(), a.owner(), a);
 		}
 	}
 
-	void dumpCode(MethodInfo m) {
+	static void dumpCode(PrintStream out, MethodInfo m) {
 		Code code = null;
 		for (AttributeInfo a : m.attributes()) {
 			if (a instanceof Code) {
@@ -75,16 +66,16 @@ public class DumpClass {
 		if (code == null) { return; }
 
 		for (Instruction insn : code.instructions()) {
-			print("\t\t%s", insn);
+			print(out, "\t\t%s", insn);
 		}
 	}
 
-	void print(String msg, Object... args) {
-		System.out.printf(msg, args);
-		System.out.println();
+	static void print(PrintStream out, String msg, Object... args) {
+		out.printf(msg, args);
+		out.println();
 	}
 
-	List<String> toNames(List<ClassRef> refs) {
+	static List<String> toNames(List<ClassRef> refs) {
 		List<String> names = new ArrayList<String>();
 		for (ClassRef cref : refs) {
 			names.add(cref.value());
@@ -92,7 +83,7 @@ public class DumpClass {
 		return names;
 	}
 
-	List<String> toNames2(List<TypeDescriptor> list) {
+	static List<String> toNames2(List<TypeDescriptor> list) {
 		List<String> names = new ArrayList<String>();
 		for (TypeDescriptor d : list) {
 			names.add(d.getTypeName());
